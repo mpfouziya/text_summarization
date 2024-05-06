dcef09556a468d14101cdf2443dc5997179de3bd
mpfouziya@gmail.com <mpfouziya@gmail.com>
2024-05-06 14:09:49 +0300

    Data Ingestion Track

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# text_summarization
+#End to End text summarization project
+
+## Workflows
+
+1. Update config.yaml file
+2. Update params.yaml file
+3. Update entity . Entity is return type of a function
+4. Update configuration manager in src/config
+5. Update components
+6. Update pipeline
+7. Update the main.py
+8. Update the app.py
+9. 
--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from src.text_summarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.text_summarization.logging import logger
-logger.info("Welcome to our custom log")
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx======x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
